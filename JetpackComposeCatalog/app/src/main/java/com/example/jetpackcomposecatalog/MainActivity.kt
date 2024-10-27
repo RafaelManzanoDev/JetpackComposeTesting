fb841d0fd91e35b1315566d2814c45d46a050e61
RafaelManzanoDev <manzanomedinarafael@gmail.com>
2024-10-27 14:39:56 +0100

    Seccion 3
    Estados en Compose

--- JetpackComposeCatalog/app/src/main/java/com/example/jetpackcomposecatalog/MainActivity.kt
+++ JetpackComposeCatalog/app/src/main/java/com/example/jetpackcomposecatalog/MainActivity.kt
@@ -18,10 +18,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -39,7 +45,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyComplexLayoutWithSpacer()
+                    MyStatePropertyExample()
                 }
             }
         }
@@ -76,25 +82,37 @@ fun MyColumn() {
 }
 
 @Composable
-fun myRow() {
-    Row(modifier = Modifier
-        .fillMaxSize()
-        .horizontalScroll(rememberScrollState())) {
-        Text(text = "Ejemplo1", modifier = Modifier
-            .background(Color.Red)
-            .width(100.dp))
-        Text(text = "Ejemplo2", modifier = Modifier
-            .background(Color.Black)
-            .width(100.dp))
-        Text(text = "Ejemplo3", modifier = Modifier
-            .background(Color.Cyan)
-            .width(100.dp))
-        Text(text = "Ejemplo4", modifier = Modifier
-            .background(Color.Green)
-            .width(100.dp))
-        Text(text = "Ejemplo5", modifier = Modifier
-            .background(Color.Magenta)
-            .width(100.dp))
+fun MyRow() {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .horizontalScroll(rememberScrollState())
+    ) {
+        Text(
+            text = "Ejemplo1", modifier = Modifier
+                .background(Color.Red)
+                .width(100.dp)
+        )
+        Text(
+            text = "Ejemplo2", modifier = Modifier
+                .background(Color.Black)
+                .width(100.dp)
+        )
+        Text(
+            text = "Ejemplo3", modifier = Modifier
+                .background(Color.Cyan)
+                .width(100.dp)
+        )
+        Text(
+            text = "Ejemplo4", modifier = Modifier
+                .background(Color.Green)
+                .width(100.dp)
+        )
+        Text(
+            text = "Ejemplo5", modifier = Modifier
+                .background(Color.Magenta)
+                .width(100.dp)
+        )
     }
 }
 
@@ -105,25 +123,29 @@ fun MyComplexLayout() {
             Modifier
                 .fillMaxWidth()
                 .weight(1f)
-                .background(Color.Cyan)) {
+                .background(Color.Cyan)
+        ) {
 
         }
 
         Row(
             Modifier
                 .fillMaxWidth()
-                .weight(1f)) {
+                .weight(1f)
+        ) {
             Box(
                 Modifier
                     .weight(1f)
                     .fillMaxHeight()
-                    .background(Color.Red))
+                    .background(Color.Red)
+            )
             Box(
                 Modifier
                     .weight(1f)
                     .fillMaxHeight()
                     .background(Color.Green),
-                contentAlignment = Alignment.Center) {
+                contentAlignment = Alignment.Center
+            ) {
                 Text(text = "Hola soy Rafael Manzano")
             }
         }
@@ -132,7 +154,8 @@ fun MyComplexLayout() {
             Modifier
                 .fillMaxWidth()
                 .weight(1f)
-                .background(Color.Magenta)) {
+                .background(Color.Magenta)
+        ) {
 
         }
     }
@@ -146,20 +169,23 @@ fun ReplicateDesign() {
                 .fillMaxWidth()
                 .weight(1f)
                 .background(Color.Cyan),
-            contentAlignment = Alignment.Center) {
+            contentAlignment = Alignment.Center
+        ) {
             Text(text = "Ejemplo 1")
         }
 
         Row(
             Modifier
                 .fillMaxWidth()
-                .weight(1f)) {
+                .weight(1f)
+        ) {
             Box(
                 Modifier
                     .weight(1f)
                     .fillMaxHeight()
                     .background(Color.Red),
-                contentAlignment = Alignment.Center) {
+                contentAlignment = Alignment.Center
+            ) {
                 Text(text = "Ejemplo 2")
             }
             Box(
@@ -167,7 +193,8 @@ fun ReplicateDesign() {
                     .weight(1f)
                     .fillMaxHeight()
                     .background(Color.Green),
-                contentAlignment = Alignment.Center) {
+                contentAlignment = Alignment.Center
+            ) {
                 Text(text = "Ejemplo 3")
             }
         }
@@ -177,7 +204,8 @@ fun ReplicateDesign() {
                 .fillMaxWidth()
                 .weight(1f)
                 .background(Color.Magenta),
-            contentAlignment = Alignment.BottomCenter) {
+            contentAlignment = Alignment.BottomCenter
+        ) {
             Text(text = "Ejemplo 4")
         }
     }
@@ -190,25 +218,29 @@ fun MyComplexLayoutWithSpacer() {
             Modifier
                 .fillMaxWidth()
                 .weight(1f)
-                .background(Color.Cyan)) {
+                .background(Color.Cyan)
+        ) {
 
         }
         MySpacer(size = 30)
         Row(
             Modifier
                 .fillMaxWidth()
-                .weight(1f)) {
+                .weight(1f)
+        ) {
             Box(
                 Modifier
                     .weight(1f)
                     .fillMaxHeight()
-                    .background(Color.Red))
+                    .background(Color.Red)
+            )
             Box(
                 Modifier
                     .weight(1f)
                     .fillMaxHeight()
                     .background(Color.Green),
-                contentAlignment = Alignment.Center) {
+                contentAlignment = Alignment.Center
+            ) {
                 Text(text = "Hola soy Rafael Manzano")
             }
         }
@@ -217,7 +249,8 @@ fun MyComplexLayoutWithSpacer() {
             Modifier
                 .fillMaxWidth()
                 .weight(1f)
-                .background(Color.Magenta)) {
+                .background(Color.Magenta)
+        ) {
 
         }
     }
@@ -227,6 +260,51 @@ fun MyComplexLayoutWithSpacer() {
 fun MySpacer(size: Int) {
     Spacer(modifier = Modifier.height(size.dp))
 }
+
+@Composable
+fun MyStateExampleRemember() {
+    val counter = remember { mutableIntStateOf(0) }
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(onClick = { counter.intValue++ }) {
+            Text(text = "Pulsar")
+        }
+
+        Text(text = "He sido pulsado ${counter.intValue} veces")
+    }
+}
+
+@Composable
+fun MyStateExampleRememberSaveable() {
+    val counter = rememberSaveable { mutableIntStateOf(0) }
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(onClick = { counter.intValue++ }) {
+            Text(text = "Pulsar")
+        }
+
+        Text(text = "He sido pulsado ${counter.intValue} veces")
+    }
+}
+
+@Composable
+fun MyStatePropertyExample() {
+    var counter by rememberSaveable { mutableIntStateOf(0) }
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Button(onClick = { counter++ }) {
+            Text(text = "Pulsar")
+        }
+
+        Text(text = "He sido pulsado $counter veces")
+    }
+}
 //endregion
 
 
@@ -234,6 +312,7 @@ fun MySpacer(size: Int) {
 @Composable
 fun GreetingPreview() {
     JetpackComposeCatalogTheme {
-        MyComplexLayoutWithSpacer()
+        MyStatePropertyExample()
     }
 }
+
